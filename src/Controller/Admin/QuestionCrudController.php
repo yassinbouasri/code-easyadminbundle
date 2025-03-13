a4b8f4bd4f407b763112e269037f4023be2452ea
yassinbouasri <79885361+yassinbouasri@users.noreply.github.com>
2025-03-13 16:52:31 +0100

    - AssociationField::autoComplete()
    - Controlling the Autocomplete Items with formatValue()
    
    - HTML IS Allowed in EasyAdmin
    
    - Controlling the Autocomplete Query

--- src/Controller/Admin/QuestionCrudController.php
+++ src/Controller/Admin/QuestionCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Question;
+use Doctrine\ORM\QueryBuilder;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
@@ -27,11 +28,23 @@ class QuestionCrudController extends AbstractCrudController
         yield IdField::new('id')
             ->onlyOnIndex();
         yield TextField::new('name');
-        yield AssociationField::new('topic');
+        yield AssociationField::new('topic')
+            ->autocomplete();
         yield TextAreaField::new('question');
         yield Field::new('votes', 'Total votes')
             ->setTextAlign('center');
-        yield AssociationField::new('askedBy');
+        yield AssociationField::new('askedBy')
+            ->autocomplete()
+            ->formatValue(static function ($value,Question $question) {
+                if (!$user = $question->getAskedBy()) {
+                    return null;
+                }
+                return sprintf('%s&nbsp;(%s)',$user->getEmail(), $user->getQuestions()->count());
+            })
+            ->setQueryBuilder(function (QueryBuilder $queryBuilder) {
+                $queryBuilder->andWhere('entity.enabled = :enabled');
+                $queryBuilder->setParameter('enabled', true);
+            });
         yield DateField::new('createdAt')
             ->onlyOnIndex();
     }
